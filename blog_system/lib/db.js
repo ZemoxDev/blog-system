d33a8fc6a190083c4c5c43478fe163dc61712774
ZemoxDev <68021511+ZemoxDev@users.noreply.github.com>
2022-03-26 14:50:35 +0100

    change_info

--- blog_system/lib/db.js
+++ blog_system/lib/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'w01c5bd6.kasserver.com',
-    user: 'd0385d56',
-    database: 'd0385d56',
-    password: 'Flol5g6h7m',
+    host: '...',
+    user: '...',
+    database: '...',
+    password: '...',
 })
 
 connection.connect();
